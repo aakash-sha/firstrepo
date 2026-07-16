aa8dbe6d4d684405bd64203c4bccc23ca5a9ce52
aakash-sha <skysharma0412@gmail.com>
2026-07-16 11:20:35 +0530

    changed

--- file_creatione.sh
+++ file_creatione.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -n random.txt ]
+if [ -n randome.txt ]
 then
 
-	touch /home/aakash/scripts/random.txt
-	cat /dev/urandom|head -10|fold -5|head > /home/aakash/scripts/random.txt
+	touch /home/aakash/scripts/randome.txt
+	cat /dev/urandom|head -10|fold -5|head > /home/aakash/scripts/randome.txt
 else
 	echo "Already present"
 fi
